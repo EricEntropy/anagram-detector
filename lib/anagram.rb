3eb08bf2eb4e716c9b338f70eb712b02b9fe12a1
Eric <contreras.eric18@gmail.com>
2021-01-28 06:04:13 -0500

    commit tree 40b104c166d906b91b5f74fd422865e522e309c4

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram 
+    def initialize(word)
+        @word = word.split("")
+    end 
+
+    attr_accessor :word
+
+    def match(list)
+        split_word = []
+        match = []
+        list.each do |x| 
+            #for each element in list, split it and put it into split_word
+            split_word = x.split("")
+            #if the splited sorted element matches the given word, add it to match
+            if split_word.sort == @word.sort
+                match << x
+            end
+        end
+        return match
+    end 
+end 
